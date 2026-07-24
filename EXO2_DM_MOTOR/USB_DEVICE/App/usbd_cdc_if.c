80a59fc0a738bffad46af0d5faf6701f3b019b27
ForStranding <805765597@qq.com>
2026-07-24 14:07:35 +0800

    第一次正式提交

--- EXO2_DM_MOTOR/USB_DEVICE/App/usbd_cdc_if.c
+++ EXO2_DM_MOTOR/USB_DEVICE/App/usbd_cdc_if.c
@@ -263,7 +263,6 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
-//	CDC_Transmit_FS(Buf, (uint16_t)(*Len));	//ªÿª∑≤‚ ‘
   /* Temporary ROS USB loopback test: return received data to the host. */
   (void)CDC_Transmit_FS(Buf, (uint16_t)(*Len));
 
